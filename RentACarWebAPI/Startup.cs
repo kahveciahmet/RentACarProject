d73f372050d8b3619083cab2aa6dc92a687fc805
ahmet <ahmettkkahveci@gmail.com>
2024-05-26 20:47:37 +0300

    Middleware added for handling errors and validations better.

--- RentACarWebAPI/Startup.cs
+++ RentACarWebAPI/Startup.cs
@@ -103,6 +103,8 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader());
 
             app.UseHttpsRedirection();
